ebf273db95e155a13bfbe0d4f3f8e27e75b1c0b2
FranciscoN3to <francisco2012jf@gmail.com>
2021-07-18 20:50:19 -0300

    teste deploy 1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json())
 
 app.get('/', (_, res) => {
-    res.json({message: 'olá'})
+    res.json({message: 'olá teste123'})
 })
 
 app.listen(3333, () => console.log('Server is running on port 8080'))
