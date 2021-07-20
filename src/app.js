4e7250baf0a9abadce6c57fa608ab5f81ff8d600
FranciscoN3to <francisco2012jf@gmail.com>
2021-07-20 20:58:19 -0300

    teste ci

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json())
 
 app.get('/', (_, res) => {
-    res.json({message: 'olá teste123 agora vai'})
+    res.json({message: 'hello world'})
 })
 
 app.listen(3333, () => console.log('Server is running on port 8080'))
